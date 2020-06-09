61ec692f022edc2d7871e8f3122d7fa7205da689
qazihashamhu <62767008+qazihashamhu@users.noreply.github.com>
2020-06-10 00:50:55 +0500

    Add files via upload

---
+++ Chapter Zero Data structure.py
@@ -0,0 +1,76 @@
+label variables:
+    $ chapter0_network = {
+    'node_1':[
+        ('Choice1',-1, [('Asad',1),('Alaina',1)],'node_1_1'),
+        ('Choice2',2, [('Asad',-1),('Alaina',-1)],'node_1_2')
+        ],
+    'node_2':[
+        ('Choice1',-2, [('Asad',1),('Alaina',1)],'node_2_1'),
+        ('Choice2',2, [('Asad',-1),('Alaina',-1)],'node_2_2'),
+        ('Choice3',1, [('Asad',1),('Alaina',1)],'node_2_3')
+        ],
+    'node_3':[
+        ('Choice1',-1, [('Amil',1)],'node_3_1'),
+        ('Choice2',-1, [('Amil',1)],'node_3_2'),
+        ('Choice3',-1, [('Amil',1)],'node_3_3')
+        ],
+    'node_3_1':[
+        ('Choice1',-1, [('Amil',1)],'node_3_1_1'),
+        ('Choice2',-1, [('Amil',1)],'node_3_1_2'),
+        ('Choice3',-1, [('Amil',1)],'node_3_1_3')
+        ],
+    'node_3_2':[
+        ('Choice1',-1, [('Amil',1)],'node_3_2_1'),
+        ('Choice2',-1, [('Amil',1)],'node_3_2_2'),
+        ('Choice3',-1, [('Amil',1)],'node_3_2_3')
+        ],
+    'node_3_3':[
+        ('Choice1',-1, [('Amil',1)],'node_3_3_1'),
+        ('Choice2',-1, [('Amil',1)],'node_3_3_2'),
+        ('Choice3',-1, [('Amil',1)],'node_3_3_3')
+        ],
+    'node_4':[
+        ('Choice1',1, [('Shanzayy',0)],'node_4_1'),
+        ('Choice2',-1, [('Shanzayy',-1)],'node_4_2'),
+        ('Choice3',-2, [('Shanzayy',1)],'node_4_3')
+        ],
+    'node_5':[
+        ('Choice1',-3, [('Shanzayy',2)],'node_5_1'),
+        ('Choice2',1, [('Shanzayy',-2)],'node_5_2')
+        ],
+    'node_6':[
+        ('Choice1',-2, [('Asad',2),('Alaina',2),('Ayesha',2),('Amil',2)],'node_6_1'),
+        ('Choice2',0, [('Asad',0),('Alaina',0),('Ayesha',0),('Amil',0)],'node_6_2'),
+        ('Choice3',4, [('Asad',-4),('Alaina',-4),('Ayesha',-4),('Amil',-4)],'node_6_3')
+        ],
+    'node_6_1':[
+        ('Choice1',-1, [('Asad',0),('Alaina',0),('Ayesha',0),('Amil',0)],'node_6_1_1'),
+        ('Choice2',-3, [('Asad',2),('Alaina',2),('Ayesha',2),('Amil',2)],'node_6_1_2'),
+        ('Choice3',2, [('Asad',-1),('Alaina',-1),('Ayesha',-2),('Amil',-1)],'node_6_1_3')
+        ],
+    'node_6_2':[
+        ('Choice1',-1, [('Asad',1),('Alaina',1),('Ayesha',1),('Amil',1)],'node_6_2_1'),
+        ('Choice2',2, [('Asad',-2),('Alaina',-2),('Ayesha',-2),('Amil',-2)],'node_6_2_2'),
+        ('Choice3',-3, [('Asad',2),('Alaina',2),('Ayesha',2),('Amil',2)],'node_6_2_3')
+        ],
+    'node_7':[
+        ('Choice1',-2, [('John',2),('Zayn',1)],'node_7_1'), 
+        ('Choice2',2, [('John',-2)],'node_7_2') 
+        ],
+    'node_8':[
+        ('Choice1',-2, [('Zayn',2)],'node_8_1'),
+        ('Choice2',-1, [('Zayn',1)],'node_8_2'),
+        ('Choice3',-1, [('Zayn',1)],'node_8_3'),
+        ('Choice4',2, [('Zayn',-2)],'node_8_4')
+        ],
+    'node_9':[
+        ('Choice1',-3, [('John',3)],'node_9_1'),
+        ('Choice2',-3, [('Zayn',3)],'node_9_2'),
+        ('Choice3',0, [('Zayn',0)],'node_9_3')
+        ],
+    'node_10':[
+        ('Choice1',-3, [('Sean',3)],'node_10_1'),
+        ('Choice2',-3, [('Sean',3)],'node_10_2'),
+        ('Choice3',0, [('Sean',0)],'node_10_3')
+        ],  
+}
